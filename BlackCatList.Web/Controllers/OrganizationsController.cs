e7ad0bcd90ed27f7b2f48814373803ede8c95d70
Evgeniya Lyubomirova <evgeniyalyubomirova1@gmail.com>
2016-08-26 00:23:37 +0300

    implemented basic organizations administration

---
+++ BlackCatList.Web/Controllers/OrganizationsController.cs
@@ -0,0 +1,132 @@
+﻿namespace BlackCatList.Web.Controllers
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Net;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+    using Models;
+    using Models.Organizations;
+
+    [Authorize(Roles = "Administrator,Moderator")]
+    public class OrganizationsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Organizations
+        public async Task<ActionResult> Index()
+        {
+            var organizations = this.db.Organizations.Include(o => o.City).Include(o => o.Country).Include(o => o.CreatedBy).Include(o => o.Image).Include(o => o.ModifiedBy).Include(o => o.Street);
+            return this.View((await organizations.ToListAsync()).Select(OrganizationsViewModel.Create));
+        }
+
+        // GET: Organizations/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Organization organization = await this.db.Organizations.FindAsync(id);
+            if (organization == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            return this.View(OrganizationsViewModel.Create(organization));
+        }
+
+        // GET: Organizations/Create
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        // POST: Organizations/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(OrganizationsViewModel organization)
+        {
+            if (this.ModelState.IsValid)
+            {
+                this.db.Organizations.Add(organization.ToEntity());
+                await this.db.SaveChangesAsync();
+                return this.RedirectToAction("Index");
+            }
+
+            return this.View(organization);
+        }
+
+        // GET: Organizations/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Organization organization = await this.db.Organizations.FindAsync(id);
+            if (organization == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            return this.View(OrganizationsViewModel.Create(organization));
+        }
+
+        // POST: Organizations/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(OrganizationsViewModel organization)
+        {
+            if (this.ModelState.IsValid)
+            {
+                this.db.Entry(organization.ToEntity()).State = EntityState.Modified;
+                await this.db.SaveChangesAsync();
+                return this.RedirectToAction("Index");
+            }
+
+            return this.View(organization);
+        }
+
+        // GET: Organizations/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Organization organization = await this.db.Organizations.FindAsync(id);
+            if (organization == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            return this.View(OrganizationsViewModel.Create(organization));
+        }
+
+        // POST: Organizations/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Organization organization = await this.db.Organizations.FindAsync(id);
+            this.db.Organizations.Remove(organization);
+            await this.db.SaveChangesAsync();
+            return this.RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BlackCatList.Web/Models/Organizations/OrganizationsViewModel.cs
@@ -0,0 +1,86 @@
+﻿namespace BlackCatList.Web.Models.Organizations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
+
+    public class OrganizationsViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Phone]
+        [MaxLength(50)]
+        public string Phone { get; set; }
+
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        public byte[] Photo { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        [Required]
+        public string Comment { get; set; }
+
+        public int? CountryId { get; set; }
+
+        public int? CityId { get; set; }
+
+        public int? StreetId { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime ModifiedOn { get; set; }
+
+        public static OrganizationsViewModel Create(Organization organization)
+        {
+            return new OrganizationsViewModel
+            {
+                Id = organization.Id,
+                Name = organization.Name,
+                Phone = organization.Phone,
+                Email = organization.Email,
+                Photo = organization.Image?.Content,
+                Comment = organization.Comment,
+                CountryId = organization.CountryId,
+                Country = organization.Country?.Name,
+                CityId = organization.CityId,
+                City = organization.City?.Name,
+                Street = organization.Street?.Name,
+                StreetId = organization.StreetId,
+                CreatedBy = organization.CreatedBy.UserName,
+                CreatedOn = organization.CreatedOn,
+                ModifiedBy = organization.ModifiedBy.UserName,
+                ModifiedOn = organization.ModifiedOn
+            };
+        }
+
+        public Organization ToEntity()
+        {
+            return new Organization
+            {
+                Id = this.Id,
+                Name = this.Name,
+                Phone = this.Phone,
+                Email = this.Email,
+                // Image = new Image { Content = this.Photo },
+                Comment = this.Comment,
+            };
+        }
+    }
+}
