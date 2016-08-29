2e1d0dcec7b44e7a62254461f1dd26c115852a17
Evgeniya Lyubomirova <evgeniyalyubomirova1@gmail.com>
2016-08-29 20:56:51 +0300

    added unobtrusive image preview for img+input combo

---
+++ BlackCatList.Web/Scripts/jquery.preview.unobtrusive.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    if (typeof FileReader === "undefined") {
+        return;
+    }
+
+    $("img + input[type='file']").change(function () {
+        previewImage(this);
+    });
+
+    function previewImage(input) {
+
+        if (input.files == null || input.files[0] == null) {
+            return;
+        }
+
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            input.previousElementSibling.src = e.target.result;
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}(jQuery));
